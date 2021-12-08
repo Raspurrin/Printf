bc138cb179ccba31df7a53617526459afd338558
Raspurrin <vondetou@gmail.com>
2021-12-08 16:37:22 +0100

    Added char conversion and unnecessary guard

--- ft_uputnbr.c
+++ ft_uputnbr.c
@@ -6,7 +6,7 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 02:10:29 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/06 22:13:54 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/06 23:37:55 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 int32_t	ft_uputnbr(uint32_t nbr)
 {
 	int32_t	i;
+	char	c;
 
 	i = 0;
+	if (nbr < 0)
+		return (0);
 	if (nbr >= 10)
 	{
-		ft_uputnbr(nbr / 10);
-		ft_uputnbr(nbr % 10);
+		i += ft_uputnbr(nbr / 10);
+		i += ft_uputnbr(nbr % 10);
 	}
 	if (nbr < 10)
-		i += ft_putchar(nbr);
+	{
+		c = nbr + '0';
+		i += ft_putchar(c);
+	}
 	return (i);
 }
