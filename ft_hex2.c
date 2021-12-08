fe26b5c7557cb0250ba54859f2288575a57f185f
Raspurrin <vondetou@gmail.com>
2021-12-08 16:31:18 +0100

    Made putmem call an entirely new hex function with size_t instead of uint32_t

--- ft_hex2.c
+++ ft_hex2.c
@@ -1,27 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putmem.c                                        :+:      :+:    :+:   */
+/*   ft_hex2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/06 21:01:19 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/08 16:18:39 by mialbert         ###   ########.fr       */
+/*   Created: 2021/12/05 02:10:29 by mialbert          #+#    #+#             */
+/*   Updated: 2021/12/08 16:18:16 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int32_t	ft_putmem(size_t *ptr, const char *hexbase)
+int32_t	ft_hex2(size_t nbr, const char *hexbase)
 {
-	int32_t			i;
-	size_t		ptr2;
+	int32_t	i;
 
 	i = 0;
-	ptr2 = (size_t)ptr;
-	if (!ptr)
-		i += ft_putstr("0x0");
-	i += ft_putstr("0x");
-	i += ft_hex2((unsigned long long int)ptr, hexbase);
+	if (nbr == 0)
+		i += ft_putchar('0');
+	if (nbr >= 16)
+	{
+		i += ft_hex2(nbr / 16, "0123456789abcdef");
+		i += ft_hex2(nbr % 16, "0123456789abcdef");
+	}
+	if (nbr < 16 && nbr != 0)
+		i += ft_putchar(hexbase[nbr]);
 	return (i);
 }
