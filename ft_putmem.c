a6f3dece8189b52b306f9e0c37d5d5d19b5e3a01
Raspurrin <vondetou@gmail.com>
2021-12-16 21:56:56 +0100

    put if condition in return

--- ft_putmem.c
+++ ft_putmem.c
@@ -6,22 +6,20 @@
 /*   By: mialbert <mialbert@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 21:01:19 by mialbert          #+#    #+#             */
-/*   Updated: 2021/12/08 16:18:39 by mialbert         ###   ########.fr       */
+/*   Updated: 2021/12/16 21:52:29 by mialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 int32_t	ft_putmem(size_t *ptr, const char *hexbase)
 {
-	int32_t			i;
-	size_t		ptr2;
+	int32_t	i;
+	size_t	ptr2;
 
 	i = 0;
 	ptr2 = (size_t)ptr;
 	if (!ptr)
-		i += ft_putstr("0x0");
-	i += ft_putstr("0x");
-	i += ft_hex2((unsigned long long int)ptr, hexbase);
-	return (i);
+		return (ft_putstr("0x0"));
+	return (ft_putstr("0x") + ft_hex2((unsigned long long int)ptr, hexbase));
 }
